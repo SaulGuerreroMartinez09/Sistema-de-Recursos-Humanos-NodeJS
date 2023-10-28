ee4da444c13dd7d914c2468a231f6694494d7738
SaulGuerreroMartinez09 <136666757+SaulGuerreroMartinez09@users.noreply.github.com>
2023-10-27 18:55:52 -0600

    update

--- RH/js/modifyusers.js
+++ RH/js/modifyusers.js
@@ -1,61 +1,114 @@
-window.onload = init 
+const link = 'http://localhost:3000/';
+const headers = {
+    'Authorization': 'bearer ' + localStorage.getItem("token")
+};
 
+window.onload = init;
 
-function init(){
-    if(localStorage.getItem("token")){
-        token = localStorage.getItem("token")
-        headers = {
-            headers: {
-                'Authorization': "bearer " + localStorage.getItem("token")
-            }
-        }
-        //cargar los datos del empleado con base en su correo
-        loadDatos()
+function init() {
+    if (localStorage.getItem("token")) {
+        document.querySelector('.btn-secondary').addEventListener('click', function () {
+            window.location.href = 'rh.html';
+        });
 
-        document.querySelector('.btn-secondary').addEventListener('click', function(){
-            window.location.href = "rh.html"
-        })
-
-        document.querySelector('.btn-primary').addEventListener('click', update)
-    } else {
-        window.location.href = "viewusers.html"
+        document.querySelector('.btn-primary').addEventListener('click', verEmpleados);
     }
 }
 
-function loadDatos(){
-    var correo = localStorage.getItem("correo")
-    axios.get("http://localhost:3000/user/viewusers/" + correo, headers).then(function(res){
-        //console.log(res)
-        //llenamos los input
-        var datos = res.data.message
-        document.querySelector('#nombre').value = `${datos[0].nombre}`
-        document.querySelector('#apellido').value = `${datos[0].apellido}`
-        document.querySelector('#email').value = `${datos[0].correo}`
-        document.querySelector('#telefono').value = `${datos[0].telefono}`
-        document.querySelector('#direccion').value = `${datos[0].direccion}`
-    }).catch(function(err){
-        console.log(err)
-    })
+function verEmpleados() {
+    if (!localStorage.getItem("token")) {
+        alert("Debes iniciar sesión para ver empleados.");
+        return;
+    }
+
+    axios.get(link + 'rh', { headers })
+        .then(function (res) {
+            const empleados = res.data;
+
+            if (empleados != null) {
+                console.log("Hasbulla")
+            }
+        })
+        .catch(function (err) {
+           console.log("Se ha producido un error al recuperar la lista de empleados. Inténtalo de nuevo más tarde.");
+            console.log(err);
+        });
 }
 
-function update(){
-    var nombre = document.getElementById('nombre').value
-    var apellido = document.getElementById('apellido').value
-    var telefono = document.getElementById('telefono').value
-    var correo = document.getElementById('email').value
-    var direccion = document.getElementById('direccion').value
+function mostrarEmpleados(empleados) {
+    const table = document.createElement('table');
+    table.classList.add('table');
+    table.innerHTML = `
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Apellido</th>
+                <th scope="col">Correo Electrónico</th>
+                <th scope="col">Teléfono</th>
+                <th scope="col">Dirección</th>
+                <th scope="col">Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    `;
 
-    axios.put('http://localhost:3000/rh', {
-            nombre: nombre,
-            apellido: apellido,
-            telefono: telefono,
-            correo: correo,
-            direccion: direccion
-    }, headers).then(function(res){
-        console.log(res)
-        alert("Modificacion exitosa")
-        window.location.href = "rh.html"
-    }).catch(function(err){
-        console.log(err)
-    })
+    const tbody = table.querySelector('tbody');
+    const empleadosData = empleados.message;
+
+    if (empleadosData.length > 0) {
+        for (let i = 0; i < empleadosData.length; i++) {
+            const empleado = empleadosData[i];
+            const empleado_id = empleado.empleado_id;
+            const nombre = empleado.nombre;
+            const apellido = empleado.apellido;
+            const correo = empleado.correo;
+            const telefono = empleado.telefono;
+            const direccion = empleado.direccion;
+
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${empleado_id}</td>
+                <td>${nombre}</td>
+                <td>${apellido}</td>
+                <td>${correo}</td>
+                <td>${telefono}</td>
+                <td>${direccion}</td>
+                <td>
+                    <button class="btn btn-delete" empId="${empleado_id}">Eliminar</button>
+                    <button class="btn btn-edit">Editar</button>
+                </td>
+            `;
+            tbody.appendChild(row);
+                const btnEliminar = row.querySelector('.btn-delete');
+                btnEliminar.addEventListener('click', () => {
+                const empleadoID = btnEliminar.getAttribute('empId');
+                eliminarEmpleado(empleadoID);
+});
+        }
+    } else {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td colspan="7">No se encontraron empleados.</td>
+        `;
+        tbody.appendChild(row);
+    }
+
+    const container = document.querySelector('.table-container');
+    container.innerHTML = '';
+    container.appendChild(table);
+}
+
+
+
+function eliminarEmpleado(empleadoID) {
+    axios.delete(link + 'rh/' + empleadoID, { headers} )
+        .then(function (res) {
+            alert("Empleado eliminado con éxito");
+            window.location.href = 'viewusers.html';
+        })
+        .catch(function (err) {
+            console.log("Error al eliminar el empleado:", err);
+        });
 }
